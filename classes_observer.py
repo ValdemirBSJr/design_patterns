20d2d348dce29c536f2ae8b28e87bb151fdba794
valdemir <badmoon25@gmail.com>
2020-01-08 09:01:05 -0300

    começando design pattern em python
    atualizado com classes

--- classes_observer.py
+++ classes_observer.py
@@ -11,3 +11,16 @@ outro em soup
 exemplo abstrato
 '''
 
+class Bruxo:
+    def bruxeis(self):
+        print('The dead are coming')
+
+class Genio:
+    def genieis(self):
+        print('Los muertos vuelven')
+
+class Deus:
+    def deuseis(self):
+        print('Do doden komen')
+
+
